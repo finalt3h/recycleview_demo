dab928abbcc3bdab12a078b88bcc19b2a6f6b2fe
SonNM <sonnm@ghtk.vn>
2022-06-07 00:33:37 +0700

    update code

--- app/src/main/java/com/ghtk/recycleview/MainActivity.java
+++ app/src/main/java/com/ghtk/recycleview/MainActivity.java
@@ -1,14 +1,11 @@
 package com.ghtk.recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.ghtk.recycleview.ui.AnimationGridActivity;
-import com.ghtk.recycleview.ui.AnimationListActivity;
 import com.ghtk.recycleview.ui.GridActivity;
 import com.ghtk.recycleview.ui.ListActivity;
 
@@ -31,16 +28,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, GridActivity.class);
         startActivity(intent);
     }
-
-    public void adapterList(View viw){
-        Intent intent = new Intent(this, AnimationListActivity.class);
-        startActivity(intent);
-    }
-
-    public void adapterGrid(View viw){
-        Intent intent = new Intent(this, AnimationGridActivity.class);
-        startActivity(intent);
-    }
-
+    
 
 }
